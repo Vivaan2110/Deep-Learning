489dd4ee6cb26248596aa28ad7316a13162d90bf
Vivaan <vivaan211005@gmail.com>
2026-02-05 22:35:16 +0530

    ISIC final model and preperation for fit

--- ISIC/Preprocessing.py
+++ ISIC/Preprocessing.py
@@ -8,6 +8,8 @@ BASE_PATH='/Volumes/Extreme SSD/ISIC-20/kagglehub/datasets/sumaiyabinteshahid/is
 
 IMAGE_PATH=BASE_PATH+"/train"
 
+CACHE_PATH=BASE_PATH+"/cache"
+
 df=pd.read_csv(BASE_PATH+'/ISIC_2020_Train_Metadata.csv')
 
 label_dict=dict(zip(df["image_name"], df["target"]))
@@ -15,7 +17,7 @@ label_dict=dict(zip(df["image_name"], df["target"]))
 paths=[]
 labels=[]
 
-for name, label in label_dict:
+for name, label in label_dict.items():
     paths.append(os.path.join(IMAGE_PATH,name+".jpg"))
     labels.append(label)
 
@@ -24,9 +26,8 @@ labels=np.array(labels)
 
 def load_image(path, label):
     img=tf.io.read_file(path)
-    img=tf.io.decode_jpeg(path)
+    img=tf.image.decode_jpeg(img, channels=3)
     img=tf.image.resize(img, (128,128))
-    img=tf.image.rgb_to_grayscale(img)
     img=tf.cast(img, tf.float32)/255.0
     
     return img, label
@@ -35,14 +36,31 @@ dataset=tf.data.Dataset.from_tensor_slices((paths,labels))
 
 dataset=(
     dataset
-    .shuffle(len(paths), seed=0)
     .map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-    .batch(32)
-    .prefetch(tf.data.AUTOTUNE)
 )
 
 TOTAL=len(paths)
 train_size=int(TOTAL*0.8)
+valid_size=int(TOTAL*0.2)
 
-train_ds=dataset.take(train_size)
-valid_ds=dataset.skip(train_size)
+train_ds=(
+    dataset
+    .take(train_size)    
+    .shuffle(2048,seed=0)
+    .batch(32)
+    .cache(CACHE_PATH)
+    .prefetch(tf.data.AUTOTUNE)
+)
+
+valid_ds=(
+    dataset
+    .skip(train_size)
+    .batch(32)
+    .cache(CACHE_PATH)
+    .prefetch(tf.data.AUTOTUNE)
+)
+
+pos_freq=labels.mean(axis=0)
+neg_freq=1.0-pos_freq
+
+pos_weights=(neg_freq/(pos_freq+1e-6)).astype(np.float32)
